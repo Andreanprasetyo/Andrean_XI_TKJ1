d680a2daa9c5a1081647e5f5ed85492ae6611015
Andreanprasetyo <149045001+Andreanprasetyo@users.noreply.github.com>
2023-11-30 08:19:10 +0700

    Create perulangan 5.py

---
+++ perulangan 5.py
@@ -0,0 +1,10 @@
+# nama : Andrean Prasetyo
+#kelas : XI TKJ 1
+#no absen : 04
+
+#soal : 
+
+waktu_total = 120  # Total waktu dalam menit
+pembelahan = waktu_total // 20  # Menggunakan operator floor division untuk menghitung pembelahan
+
+print(f"Jumlah pembelahan bakteri dalam rentang waktu 2 jam adalah {pembelahan} kali.")
